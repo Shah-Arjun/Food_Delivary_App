211176dd9e406930983bdaaa178b5a28c0c98f7c
Arjun shah <arjunsah856@gmail.com>
2025-06-13 15:42:46 +0545

    modified

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,14 +5,16 @@ import Card from "../components/Card";
 import Carousel from "../components/Carousel"
 
 export default function Home() {
-    return (
-      <div>
-        <div> <Navbar /> </div>
-        <div> <Carousel /> </div>
-        <div className="m-3">
-          <Card /> 
-        </div>
-        <div> <Footer /> </div> 
+  return (
+    <div>
+      <div> <Navbar /> </div>
+      <div> <Carousel /> </div>
+      <div className="m-3">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div> <Footer /> </div>
     </div>
-    );
+  );
 }

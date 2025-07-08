a613ee70f77d384d909e552522141566098b3fae
Arjun shah <arjunsah856@gmail.com>
2025-07-08 22:08:43 +0545

    Added Login, SignUp logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-success">
                 <div className="container-fluid">
-                    <a className="navbar-brand fs-1 fst-italic" href="/">
+                    <Link className="navbar-brand fs-1 fst-italic" to="/">
                         GoFood
-                    </a>
+                    </Link>
                     <button
                         className="navbar-toggler"
                         type="button"
@@ -20,23 +21,24 @@ export default function Navbar() {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
+                        <ul className="navbar-nav me-auto mb-2">
                             <li className="nav-item">
-                                <a className="nav-link" aria-current="page" href="/">
-                                    Home
-                                </a>
+                                <Link className="nav-link active fs-5" aria-current="page" to="/"> Home </Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/login">
-                                    Login
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/creatuser">
-                                    Signup
-                                </a>
-                            </li>
+                            {(localStorage.getItem("authToken")) ?
+                                <li className="nav-item">
+                                    <Link className="nav-link active fs-5" aria-current="page" to="/"> My Orders </Link>
+                                </li>
+                            :""
+                            }
+
                         </ul>
+
+                        <div className="d-flex">
+                            <Link className="btn bg-white text-success mx-1" href="/login">Login</Link>
+                            <Link className="btn bg-white text-success mx-1" href="/creatuser">SignUp</Link>
+                        </div>
+
                     </div>
                 </div>
             </nav>

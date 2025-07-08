a9340fdedeb133da2e454e487799ad408cdb47d6
Arjun shah <arjunsah856@gmail.com>
2025-07-08 18:01:19 +0545

    modified card

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ export default function Card(props) {
         <div
           className="card mt-3"
           style={{ width: "18rem", maxHeight: "360px" }}>
-          <img src={props.imgSrc} alt="Indian food" className="card-img-top"/>
+          <img src={props.imgSrc} alt="Indian food" className="card-img-top" style={{height:"120px", objectFit:"fill"}} />
           <div className="card-body">
             <h5 className="card-title">{props.foodName}</h5>
             <div className="container w-100">
